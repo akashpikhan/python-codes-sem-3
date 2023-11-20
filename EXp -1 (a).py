bfe00c0cabd880a49be71b0dc24c594dfc593275
akashpikhan <121306850+akashpikhan@users.noreply.github.com>
2023-11-20 17:55:56 +0530

    Add files via upload

---
+++ EXp -1 (a).py
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      asus
+#
+# Created:     20-11-2023
+# Copyright:   (c) asus 2023
+# Licence:     <your licence>
+#------------------------------------------------------------------------------
+# DataType Output: str
+x = "Welcome to CSE Department"
+print("DataType Output:", type(x))
+
+# DataType Output: int
+x = 50
+print("DataType Output:", type(x))
+
+# DataType Output: float
+x = 60.5
+print("DataType Output:", type(x))
+
+# DataType Output: complex
+x = 3j
+print("DataType Output:", type(x))
+
+# DataType Output: list
+x = ["Python", "Programming", "Language"]
+print("DataType Output:", type(x))
+
+# DataType Output: tuple
+x = ("MIET", "CSE", "Students")
+print("DataType Output:", type(x))
+
+# DataType Output: range
+x = range(10)
+print("DataType Output:", type(x))
+
+# DataType Output: dict
+x = {"name": "Kartik", "age": 21}
+print("DataType Output:", type(x))
+
+# DataType Output: set
+x = {"Data", "Types", "Python"}
+print("DataType Output:", type(x))
+
+
